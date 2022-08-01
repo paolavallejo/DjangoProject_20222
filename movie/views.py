058f15a87e2fe360f1aa7655bdf79867ceb923ac
paolavallejo <pvallej3@eafit.edu.co>
2022-08-01 14:32:09 -0500

    cambios en la funcion home.

--- movie/views.py
+++ movie/views.py
@@ -6,5 +6,5 @@ from django.http import HttpResponse
 def home(request):
     #return HttpResponse('<h1>Welcome to Home Page</h1>')
     #return render(request, 'home.html')
-    return render(request, 'home.html', {'name':'Greg Lim'})
+    return render(request, 'home.html', {'name':'Paola Vallejo'})
 
